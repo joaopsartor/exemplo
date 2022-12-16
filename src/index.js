cc1c0b3488ee3d1c841331617cdf92f994639f31
joaopsartor <joaosartor@gmail.com>
2022-12-16 14:07:29 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/*const http = require("http")
+
+const hostname = "localhost"
+const port = 3333
+
+const server = http.createServer((req,res)=>{
+    res.setHeader("contect-type","text/plain")
+    res.end("Hello World")
+})
+
+server.listen(port, hostname, ()=>{
+    console.log("Servidor Rodando.")
+})*/
+
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const router = express.Router()
+
+router.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+
+router.get("/contato", (req,res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+app.use(router)
+
+app.listen(3333, ()=>{
+    console.log("SERVIDOR RODANDO")
+})
